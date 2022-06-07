1d054ed1ff2d3ee5a63d36e888835e2b8063de31
C0llin5 <ebongcollins@gmail.com>
2022-06-07 07:30:53 -0700

    add file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "_putchar.h"
+
+/**
+* main - entry point
+*
+* Description: Prints Holberton with _putchar
+*
+* Return: always 0
+*/
+
+int main(void)
+{
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+	return (0);
+}
