e72b9084fdab7ac569563addbac1af143ee6dea1
C0llin5 <ebongcollins@gmail.com>
2022-06-09 12:08:52 -0700

    add files

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_most_numbers - print numbrs 0 to 9 except 2 and 4
+* Return: values
+*/
+void print_most_numbers(void)
+{
+	int ch = 0;
+
+	while (ch < 10)
+	{
+		if (ch != 2 && ch != 4)
+		{
+			_putchar(ch + '0');
+		}
+		ch += 1;
+	}
+	_putchar('\n');
+}
