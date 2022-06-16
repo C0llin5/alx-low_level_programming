7bbcb2b7bba5a61838106b4b2129ceade76ea261
C0llin5 <ebongcollins@gmail.com>
2022-06-16 13:10:32 -0700

    add file

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,5 +6,20 @@
  */
 char *rot13(char *str)
 {
+	char regALPHA[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char altALPHA[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; regALPHA[j] != '\0'; j++)
+		{
+			if (str[i] == regALPHA[j])
+			{
+				str[i] = altALPHA[j];
+				break;
+			}
+		}
+	}
 	return (str);
 }
